4ecb085aabc7e4969a75423a15c35fe6e91213a3
Vadim <dolinenko2006@ya.ru>
2022-12-06 07:21:55 +0300

    ne znaju chto

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+# 10.1.11 работа через терминал
+# git add chapter1.html — добавит файл
+# git add . — добавит все в текущей папке
+# git commit -m "changed library because outdated"
+# Атрибут -m или —message указывает на то, что дальше будет сообщение коммита.
+# Именно оно и следует дальше в кавычках.
+# use "git restore <file>..." to discard changes in working directory)
+# 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
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# git remote add origin https://github.com/dolinenko2006/lessen.git
+# git branch -M main
+# git push -u origin main
